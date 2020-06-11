4e63c3ffb0e631efd93f11011c0dac5a6a074216
OnurMorcagok <onurmorcagok@gmail.com>
2020-06-11 21:26:31 +0300

    JS Notes Added

---
+++ ES6+ Properties/Arrow Function/app.js
@@ -0,0 +1,39 @@
+// Arrow Function Using
+
+/*
+
+const name = function() {
+    console.log("Hello Onur");
+}
+
+name();
+
+const name = function (firstName,lastName) {
+    console.log("Hello",firstName,lastName);
+}
+
+name("David","Beckham");
+
+const hello = () => {
+    console.log("Hello Onur");
+}
+
+hello();
+
+const hello = () => console.log("Hello Onur");
+
+hello();
+
+Eğer tek bir parametre döndürülecekse tek bir satırda tanımlama gerçekleştirebiliriz.
+
+const cube = function (x) {
+    return x * x * x;
+}
+
+console.log("Result: " + cube(5));
+
+const cube = x => x * x * x;
+
+console.log(cube(4));
+
+*/

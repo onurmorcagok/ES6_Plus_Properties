b52a743972b90b54a08bbe2717739a5aa54acf2b
OnurMorcagok <onurmorcagok@gmail.com>
2020-06-11 22:36:16 +0300

    Notes added

---
+++ ES6+ Properties/Spread Opeator/app.js
@@ -0,0 +1,30 @@
+// Spread Operator
+
+/*
+
+const langs = ["Java", "Python", "C+++", "php"];
+
+console.log(langs[0],langs[1],langs[2]);
+
+console.log(...langs); // Spread Operator
+
+const langs2 = ["HTML5","CSS3","JavaScript",langs[0],langs[1],langs[2]];
+const langs2 = ["HTML5", "CSS3", "JavaScript", ...langs];
+
+console.log(langs2);
+
+const numbers = [1,2,3,4,5,6,7,8,9];
+
+const [a,b,...number2] = numbers;
+
+console.log(a,b);
+console.log(number2);
+
+
+const addNumbers = (a, b, c) => console.log(a + b + c);
+
+const numbers = [10, 30, 50];
+
+addNumbers(...numbers);
+
+*/

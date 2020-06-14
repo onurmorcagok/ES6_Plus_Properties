f6240efd336c69da9c7484def534e8b6251649e4
OnurMorcagok <onurmorcagok@gmail.com>
2020-06-14 15:56:32 +0300

    Update app.js

--- ES6+ Properties/Maps/app.js
+++ ES6+ Properties/Maps/app.js
@@ -25,8 +25,57 @@ const key3 = () => 4;
 
 // console.log(myMap.size);
 
+// const cities = new Map();
+
+// cities.set("İstanbul", 34);
+// cities.set("Ankara", 06);
+// cities.set("İzmir", 35);
+
+// forEach Loop
+
+// cities.forEach(function (value, key) {
+//     console.log(key, value);
+// })
+
+// For Of Loop
+
+// for (let [key, value] of cities) { // Destruction Structure
+//     console.log(key, value);
+// }
+
+// Map Keys
+
+// for (let key of cities.keys()) {
+//     console.log("Key: " + key);
+// }
+
+// Map Values
+
+// for (let value of cities.values()) {
+//     console.log("Value: " + value);
+// }
+
+// const array = [
+//     ["key1", "value1"],
+//     ["key2", "value2"]
+// ];
+
+// const lastMap = new Map(array); // Array'den Map oluşturduk
+
+// console.log(lastMap);
+
+// Maplerden Array Oluşturma
+
 const cities = new Map();
 
 cities.set("İstanbul", 34);
 cities.set("Ankara", 06);
 cities.set("İzmir", 35);
+
+const array = Array.from(cities); // Cities Map'ini Array'e dönüştürdük.
+
+// Burada dizi içerisinde dizi mevcut.
+
+// [["İstanbul",34],["Ankara",06],["İzmir",35]]
+
+console.log(array);

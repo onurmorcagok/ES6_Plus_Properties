61e41ec586da0d38fed30d97208864bc82d2d46c
OnurMorcagok <onurmorcagok@gmail.com>
2020-06-14 16:43:06 +0300

    Notes added

---
+++ ES6+ Properties/Reference Type/app.js
@@ -0,0 +1,33 @@
+const person = {
+    name: "Onur M.",
+    age: 24,
+    title: "Software Developer"
+};
+
+const person2 = {
+    name: "Onur M.",
+    age: 24,
+    title: "Software Developer"
+};
+
+// console.log(person);
+// console.log(person2);
+
+let a = "Onur Morcagok";
+let b = "Onur Morcagok";
+
+// console.log(a);
+// console.log(b);
+
+// Type Control
+
+// if (a === b) { 
+//     console.log("True");
+// }
+
+// if (person === person2){
+//     console.log("True");
+// }
+
+// Reference Type bellekte adres tuttuğu/gösterdiği için içerikler aynı dahi olsa
+// adres değeri farklı olduğu için Primitive Type arasında fark vardır.

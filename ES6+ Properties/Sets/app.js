9fe176a286a771b761704fca4a59bd3a2a138f5e
OnurMorcagok <onurmorcagok@gmail.com>
2020-06-14 16:24:55 +0300

    Notes added

---
+++ ES6+ Properties/Sets/app.js
@@ -0,0 +1,17 @@
+// Sets - Kümeler
+
+// Bir değeri yalnızca bir defa taşıması adına kullanılır.
+
+const mySet = new Set();
+
+mySet.add(100);
+mySet.add(100);
+mySet.add(3.14);
+mySet.add("Onur");
+mySet.add(true);
+mySet.add([1,2,3]);
+mySet.add({a:10,b:20,c:30});
+
+
+
+console.log(mySet);

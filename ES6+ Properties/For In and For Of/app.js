8bfa75a8dd0416fe757ef92a855bf76c257d511e
OnurMorcagok <onurmorcagok@gmail.com>
2020-06-14 14:58:32 +0300

    Update app.js

--- ES6+ Properties/For In and For Of/app.js
+++ ES6+ Properties/For In and For Of/app.js
@@ -1,9 +1,51 @@
 const person = {
     name: "Onur MORCAGÖK",
-    age:24,
-    salary:5000
+    age: 24,
+    salary: 5000
 };
 
-const langs = ["Java","Python","C++","php"];
+const langs = ["Java", "Python", "C++", "php", "JavaScript", "React", "Angular", "Vue"];
 
 const lastName = "Morcagok";
+
+/*
+
+For In
+
+For In döngüsü ile Object içerisinde gezinebiliriz.
+
+for (let prop in person){
+    console.log(person);
+}
+
+for (let prop in person){
+    console.log(prop,person[prop]);
+}
+
+for (let lang in langs){
+    console.log(lang);
+}
+
+for (let lang in langs){
+    console.log(langs[value]);
+}
+
+for (let value in lastName) {
+    console.log(value,lastName[value]);
+}
+
+For Of
+
+for (let prop of person){
+    console.log(person);
+}
+
+for (let value of langs){
+    console.log(value); // Dizideki degerler uzerinden gezebiliriz.
+}
+
+for (let value of lastName) {
+    console.log(value);
+}
+
+*/

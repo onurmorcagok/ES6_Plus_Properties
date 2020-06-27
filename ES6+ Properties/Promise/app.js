a642ba6a65323fefe3b5d902929e632e3b0197c6
OnurMorcagok <onurmorcagok@gmail.com>
2020-06-28 00:22:32 +0300

    Update app.js

--- ES6+ Properties/Promise/app.js
+++ ES6+ Properties/Promise/app.js
@@ -1,3 +1,4 @@
+/*
 const asyncFunction = (number, callback) => {
 
     const result = number * 2;
@@ -13,7 +14,47 @@ asyncFunction(5, function (result) {
         asyncFunction(newResult, function (lastResult) {
             console.log("Last Result: " + lastResult);
         })
-    })
+    })  
+});
+*/
 
+/*
+const coffeeOrder = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(true);
+        }, 3000)
+    })
+}
+
+const cheesecakeOrder = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(true);
+        }, 1000)
+    })
+}
+
+const prepareOrder = () => {
+
+    let coffeePrepare = false;
+    let cheesecakePrepare = false;
     
-})
+    console.log("Kahve Hazırlanıyor..");
+
+    coffeeOrder().then((coffeeStatus) => {
+        coffeePrepare = coffeeStatus;
+        console.log("Kahveniz hazır henüz servis edilmedi..");
+
+        console.log("Cheesecake Hazırlanıyor..");
+        cheesecakeOrder().then((cheesecakeStatus) => {
+            console.log("Chessecake Hazır..");
+            cheesecakePrepare = cheesecakeStatus;
+            console.log(`Kahve hazır mı: ${coffeePrepare}, Chessecake hazır mı: ${cheesecakeStatus}`);
+        })
+    })
+}
+
+prepareOrder();
+*/
+
